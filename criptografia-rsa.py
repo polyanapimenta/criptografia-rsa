17b2fd4551bf3527ba7ab6ceb00e7210ac05bd76
polyanapimenta <polyana.pimenta@gmail.com>
2018-03-13 19:13:08 -0300

    numeros-primos fucionando

--- criptografia-rsa.py
+++ criptografia-rsa.py
@@ -1,21 +1,22 @@
 from random import randint
-### Etapa 1 - Escolher p e q aleatóriamente(números primos) e calcular N=p.q ###
 
-# Verificando se p e q são números primos:
 primo = False
-count = 0
-while (primo == False):
-    print(count,':')
-    # Gerando números aleatórios p e q em Python
-    p = (randint(0, 1000))
-    q = (randint(0, 1000))
 
-    if(p > 1) and (p % 2 != 0) and (p % 3 != 0) and (p % 5 != 0) and (p % 7 != 0) or (p == 2) or (p == 3) or (p == 5) or (p == 7):
-        # 1. um número natural é primo se ele é maior que 1
-        # 2. números pares não são primos, apenas o 2
-        # 3. números múltiplos de 3 não podem ser primos, exceto o 3
-        # 4. números múltiplos de 5 não podem ser primos, exceto o 5
-        # 5. números múltiplos de 7 não podem ser primos, exceto o 7
-        print('p: ', p)
-        primo = True
-    count+=1
+while not primo:
+    p = (randint(1, 1000))
+    if(p > 1):
+        div = 0
+        for i in range(p):
+            if(i>=1) and (p % i == 0):
+                div += 1
+
+        if(p % 2 != 0) and (div <= 1):
+            primo = True
+            print('primo: ',p)
+
+
+
+
+
+
+
